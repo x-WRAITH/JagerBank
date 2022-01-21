29e30a904f63113fa7e5d9e721577f53bb466587
x-WRAITH <admin@gramyse.pl>
2022-01-21 18:26:51 +0100

    asdas

--- BankApplication/BankApplication/FormMainMenu.cs
+++ BankApplication/BankApplication/FormMainMenu.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using BankApplication.Forms;
 using FontAwesome.Sharp;
 using ClassLibrary;
 using TechSupport;
-using config_library;
 
 namespace BankApplication
 {
